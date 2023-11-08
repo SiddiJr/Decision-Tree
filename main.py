519c9b2345d767a08746efe82bb070e28cfc24ca
sidjr <sidnei_1998@hotmail.com>
2023-11-07 22:11:15 -0300

    atualização assinatura do método

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ data = pd.read_csv('datasets/data_20x20_42vic/sinais_vitais.txt', names=col_name
 data = data[['qualPres', 'pulso', 'resp', 'clas_grav']]
 df = pd.read_csv('datasets/data_800vic/sinais_vitais_sem_label.txt', names=col_names, index_col = None)
 df = df[['qualPres', 'pulso', 'resp']]
+y = pd.read_csv('datasets/data_800vic/sinais_vitais_com_label.txt', names=col_names, index_col = None)
+y = y[['clas_grav']]
 
-def fitting(X, y, criterion, splitting, mdepth, pred):
+def fitting(X, y, criterion, splitting, mdepth, X_pred, y_pred):
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
     model = tree.DecisionTreeClassifier(
                                         criterion=criterion,
                                         splitter=splitting,
                                         max_depth=mdepth,
+                                        max_leaf_nodes=6,
                                         random_state=0
                                         )
     
     clf = model.fit(X_train, y_train)
 
     pred_label_train = model.predict(X_train)
-    pred_label_test = model.predict(X_test)
-
-    pred2 = model.predict(pred)
+    pred_label_test = model.predict(X_pred)
     
 
     print('*************** Tree Summary ***************')
@@ -41,7 +43,7 @@ def fitting(X, y, criterion, splitting, mdepth, pred):
 
     score_te = model.score(X_test, y_test)
     print('Accuracy Score: ', score_te)
-    print(classification_report(y_test, pred_label_test, zero_division=0))
+    print(classification_report(y_pred, pred_label_test, zero_division=0))
     print('--------------------------------------------------------')
     print("")
 
@@ -52,4 +54,4 @@ def fitting(X, y, criterion, splitting, mdepth, pred):
     print('--------------------------------------------------------')
 
 
-fitting(data[['qualPres', 'pulso', 'resp']], data['clas_grav'], 'entropy', 'best', mdepth=3, pred = df)
+fitting(data[['qualPres', 'pulso', 'resp']], data['clas_grav'], 'entropy', 'random', mdepth=4, X_pred = df, y_pred = y)
